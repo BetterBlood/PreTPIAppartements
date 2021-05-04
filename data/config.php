0559a7598e5598b5bea27fdbaf4c82c50ecb0836
Dahïr <Better_Blood@hotmail.com>
2021-05-04 11:09:03 +0200

    Config modif
    modification du fichier de config, ajout des paths pour les fichiers de log

--- data/config.php
+++ data/config.php
@@ -7,6 +7,14 @@ $GLOBALS['MM_CONFIG'] = array(
         "host" => "localhost",
         "port" => "3306",
         "charset" => "utf8"
+    ),
+    "logPath" => array(
+        "error" => "data/Logs/errors/errorLogTest.log",
+        "appModification" => "data/Logs/DataModifications/appartements.log",
+        "useModification" => "data/Logs/DataModifications/users.log",
+        "wishModification" => "data/Logs/DataModifications/wishLists.log",
+        "debug" => "data/Logs/TMP/debug.log",
+        "activity" => "data/Logs/activity.log",
     )
 )
 ?>
